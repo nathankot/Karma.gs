f73c3391e810871b54905b6be93bda6128ebebab
Nathan Kot <me@nathankot.com>
2013-11-05 16:35:50 +1300

    setup grunt build.

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      dist: {
+        src: [
+          'src/defaults.styl', 
+          'src/utilities.styl', 
+          'src/layouting.styl', 
+          'src/typography.styl'
+        ],
+        dest: 'dist/karma.styl'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  // Default task(s).
+  grunt.registerTask('default', ['concat']);
+
+};
