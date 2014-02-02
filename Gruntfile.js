bdfab063b0532de5c446cb4ba67ecfd7ac7a50d2
Nathan Kot <me@nathankot.com>
2014-02-02 16:43:18 +0800

    Concat everything.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,30 +5,16 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     concat: {
       dist: {
-        src: [
-          'src/defaults.styl', 
-          'src/utilities.styl', 
-          'src/layouting.styl', 
-          'src/typography.styl',
-          'src/reset.styl'
-        ],
+        src: 'src/*.styl',
         dest: 'dist/karma.styl'
-      }
-    },
-    copy: {
-      dist: {
-        files: {
-          'dist/stubs.styl': 'src/stubs.styl'
-        }
       }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task(s).
-  grunt.registerTask('default', ['concat', 'copy']);
+  grunt.registerTask('default', ['concat']);
 
 };
